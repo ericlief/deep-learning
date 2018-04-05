5a146187af02b442a32aa2dccf1525aa6a4b8d12
Eric Lief <ericlief@bossa>
2018-04-05 15:34:32 +0200

    newer uppercase with size layers

--- 03/run.sh
+++ 03/run.sh
@@ -1,4 +1,7 @@
 #ulimit -t unlimited && nice -n 19 python3 uppercase.py --window 4 --activation relu --optimizer Adam --dropout .4 
 #ulimit -t unlimited && nice -n 19 python3 uppercase.py --window 4 --activation relu --optimizer Adam --dropout .4 --layers 2
 #ulimit -t unlimited && nice -n 19 python3 uppercase.py --window 6 --activation relu --optimizer Adam --dropout .4 --layers 2
-ulimit -t unlimited && nice -n 19 python3 uppercase.py --window 8 --activation relu --optimizer Adam --dropout .4 --layers 4
+#ulimit -t unlimited && nice -n 19 python3 uppercase.py --window 8 --activation relu --optimizer Adam --dropout .4 --layers 4
+ulimit -t unlimited && nice -n 19 python3 uppercase.py --window 6 --activation relu --optimizer Adam --dropout .4 --layers 1
+ulimit -t unlimited && nice -n 19 python3 uppercase.py --window 8 --activation relu --optimizer Adam --dropout .4 --layers 2
+ulimit -t unlimited && nice -n 19 python3 uppercase.py --window 8 --activation relu --optimizer Adam --dropout .4 --layers 1
