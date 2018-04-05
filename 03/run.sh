2b1a524c7cd7eb38f2e2d933cab32a371260db93
Eric Lief <liefe@u-pl2.ms.mff.cuni.cz>
2018-04-05 13:48:58 +0200

    logs

--- 03/run.sh
+++ 03/run.sh
@@ -1,3 +1,4 @@
-python3 uppercase.py --window 4 --activation relu --optimizer Adam --dropout .4 
-python3 uppercase.py --window 4 --activation relu --optimizer Adam --dropout .4 --layers 2
-python3 uppercase.py --window 6 --activation relu --optimizer Adam --dropout .4
+#ulimit -t unlimited && nice -n 19 python3 uppercase.py --window 4 --activation relu --optimizer Adam --dropout .4 
+#ulimit -t unlimited && nice -n 19 python3 uppercase.py --window 4 --activation relu --optimizer Adam --dropout .4 --layers 2
+#ulimit -t unlimited && nice -n 19 python3 uppercase.py --window 6 --activation relu --optimizer Adam --dropout .4 --layers 2
+ulimit -t unlimited && nice -n 19 python3 uppercase.py --window 8 --activation relu --optimizer Adam --dropout .4 --layers 4
