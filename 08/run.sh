9d864778fab974d747de96531889f908a59d95b4
Eric Lief <liefe@u-pl1.ms.mff.cuni.cz>
2018-05-23 09:06:28 +0200

    logs

--- 08/run.sh
+++ 08/run.sh
@@ -9,5 +9,26 @@
 #ulimit -t unlimited && nice -n 19 java -cp ../bin LPTabSeqTest
 
 
-ulimit -t unlimited && nice -n 19 python3 tagger_sota.py --rnn_cell GRU --rnn_cell_dim 128 --we_dim 64 --cle_dim 32 --epochs 10 
+# ulimit -t unlimited && nice -n 19 python3 tagger_sota.py --rnn_cell LSTM --rnn_cell_dim 200 --we_dim 64 --cle_dim 32 --epochs 10
+# ulimit -t unlimited && nice -n 19 python3 tagger_sota.py --rnn_cell LSTM --rnn_cell_dim 200 --we_dim 64 --cle_dim 32 --epochs 10 --bn True
+#limit -t unlimited && nice -n 19 python3 tagger_sota.py --rnn_cell LSTM --rnn_cell_dim 200 --we_dim 64 --cle_dim 32 --epochs 10 --bn True 
+
+
+
+#ulimit -t unlimited && nice -n 19 python3 tagger_sota.py --rnn_cell LSTM --rnn_cell_dim 200 --we_dim 64 --cle_dim 32 --epochs 10 --bn False --dropout .2
+#ulimit -t unlimited && nice -n 19 python3 tagger_sota.py --rnn_cell LSTM --rnn_cell_dim 200 --we_dim 64 --cle_dim 32 --epochs 10 --bn False --dropout .4
+ulimit -t unlimited && nice -n 19 python3 tagger_sota.py --rnn_cell LSTM --rnn_cell_dim 200 --we_dim 64 --cle_dim 32 --epochs 10 --bn True  --dropout .2
+#ulimit -t unlimited && nice -n 19 python3 tagger_sota.py --rnn_cell LSTM --rnn_cell_dim 200 --we_dim 64 --cle_dim 32 --epochs 10 --bn True  --dropout .4
+
+# ulimit -t unlimited && nice -n 19 python3 tagger_sota.py --rnn_cell GRU --rnn_cell_dim 200 --we_dim 64 --cle_dim 32 --epochs 10
+# ulimit -t unlimited && nice -n 19 python3 tagger_sota.py --rnn_cell GRU --rnn_cell_dim 200 --we_dim 64 --cle_dim 32 --epochs 10 --bn True
+# ulimit -t unlimited && nice -n 19 python3 tagger_sota.py --rnn_cell GRU --rnn_cell_dim 200 --we_dim 64 --cle_dim 32 --epochs 10 --bn True --droppout .1
+# ulimit -t unlimited && nice -n 19 python3 tagger_sota.py --rnn_cell GRU --rnn_cell_dim 200 --we_dim 64 --cle_dim 32 --epochs 10 --bn True  --droppout .2
+# ulimit -t unlimited && nice -n 19 python3 tagger_sota.py --rnn_cell GRU --rnn_cell_dim 200 --we_dim 64 --cle_dim 32 --epochs 10 --bn True  --droppout .3
+# ulimit -t unlimited && nice -n 19 python3 tagger_sota.py --rnn_cell GRU --rnn_cell_dim 200 --we_dim 64 --cle_dim 32 --epochs 10 --dropout .4 --bn True
+
+
+
+
+
 
